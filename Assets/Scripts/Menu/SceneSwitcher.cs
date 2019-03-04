e0835c849234870e5dd2c6793a34ae7b8314497e
Jake Hackett <jhackett@carbonblack.com>
2019-03-04 15:38:37 -0500

    start new game at level 1 on new game click

---
+++ Assets/Scripts/Menu/SceneSwitcher.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneSwitcher:MonoBehaviour {
+   public void NewGame(string level) {
+        SceneManager.LoadScene(level);
+   }
+}
