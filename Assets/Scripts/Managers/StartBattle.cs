d889c7964cdd92c6a870a1e0f18e6095250cb551
Jim LoRusso <jimmy.lorusso@gmail.com>
2019-03-05 01:32:13 -0500

    Add HUDCanvas to battle level

---
+++ Assets/Scripts/Managers/StartBattle.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class StartBattle : MonoBehaviour
+{
+    public int startSeconds = 3;
+    public Text textComponent;
+    private float timeLeft;
+
+    void Start()
+    {
+        timeLeft = startSeconds;
+    }
+
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        textComponent.text = (timeLeft).ToString("0");
+        if (timeLeft < 0)
+        {
+            textComponent.text = "FIGHT!!!";
+        }
+    }
+}
