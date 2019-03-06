e5ef4db7c2a03443209800dae1f2cbbd281a1470
Jake Hackett <jhackett@carbonblack.com>
2019-03-06 12:17:49 -0500

    initial progress at character selection screen

---
+++ Assets/Scripts/Menu/PlayerSelection.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSelection : MonoBehaviour {
+    private List<GameObject> players;
+    private int selectionIndex = 0;
+
+    private void Start() {
+        players = new List<GameObject>();
+
+        foreach(Transform t in transform) {
+            players.Add(t.gameObject);
+            t.gameObject.SetActive(false);
+        }
+
+        players[selectionIndex].gameObject.SetActive(true);
+    }
+}
