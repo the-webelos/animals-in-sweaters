70e202f633b6db12590b945578c18b39346a2b2c
Jake Hackett <jhackett@carbonblack.com>
2019-03-06 12:37:02 -0500

    finish character selection scene, need to link to main menu and levels still

--- Assets/Scripts/Menu/PlayerSelection.cs
+++ Assets/Scripts/Menu/PlayerSelection.cs
@@ -16,4 +16,13 @@ public class PlayerSelection : MonoBehaviour {
 
         players[selectionIndex].gameObject.SetActive(true);
     }
+
+    public void Select(int index) {
+        if (index == selectionIndex || index < 0 || players.Count <= index)
+            return;
+
+        players[selectionIndex].gameObject.SetActive(false);
+        players[index].gameObject.SetActive(true);
+        selectionIndex = index;
+    }
 }
