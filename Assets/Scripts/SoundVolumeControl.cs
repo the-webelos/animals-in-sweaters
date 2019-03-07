cd55ce86c572f6c156ad246ebd93736c2a7c236e
edmar <egoncalves@carbonblack.com>
2019-03-07 11:18:39 -0500

    refactoring script

--- Assets/Scripts/SoundVolumeControl.cs
+++ Assets/Scripts/SoundVolumeControl.cs
@@ -1,35 +1,24 @@
 ﻿using UnityEngine;
 
 public class SoundVolumeControl : MonoBehaviour {
-
-    // Reference to Audio Source component
     private AudioSource musicAudioSrc;
     private AudioSource sfxAudioSrc;
 
-    // Music volume variable that will be modified
-    // by dragging slider knob
+    // defaults
     private float musicVolume = 0.5f;
     private float sfxVolume = 0.8f;
 
-    // Use this for initialization
     void Start () {
-
-        // Assign Audio Source component to control it
+        // TODO make sure sources are being assigned correctly
         musicAudioSrc = GetComponent<AudioSource>();
         sfxAudioSrc = GetComponent<AudioSource>();
     }
-    
-    // Update is called once per frame
-    void Update () {
 
-        // Setting volume option of Audio Source to be equal to musicVolume
+    void Update () {
         musicAudioSrc.volume = musicVolume;
         sfxAudioSrc.volume = sfxVolume;
     }
 
-    // Method that is called by slider game object
-    // This method takes vol value passed by slider
-    // and sets it as musicValue
     public void SetMusicVolume(float vol)
     {
         musicVolume = vol;
