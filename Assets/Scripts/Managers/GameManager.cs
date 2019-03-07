049a14e8c178de98ca68c60d0b6557ec673cfecb
Jake Hackett <jhackett@carbonblack.com>
2019-03-07 12:06:15 -0500

    character selection affects which prefabs spawn

---
+++ Assets/Scripts/Managers/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance = null;
+    public PlayerManager playerManager;
+    public int numberOfPlayers = 2;
+
+    public GameObject[] playerPrefabs;
+
+    void Awake() {
+        if (instance == null) {
+            instance = this;
+        } else if (instance != this) {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        instance.playerManager = GetComponent<PlayerManager>();
+        instance.playerPrefabs = new GameObject[numberOfPlayers];
+    }
+
+    public void ChangePlayerPrefab(int player, GameObject prefab) {
+        Debug.Log("Player prefabs " + playerPrefabs.ToString());
+        instance.playerPrefabs[player] = prefab;
+    }
+}
