4c7018d310ec4abcf46ac0b62ffe2cdd799f3e89
Nicholas Murdock <nmurdock@carbonblack.com>
2019-03-07 21:43:08 -0600

    add island to chess level

--- Assets/Scripts/Camera/LevelIntro.cs
+++ Assets/Scripts/Camera/LevelIntro.cs
@@ -5,55 +5,60 @@ using UnityEngine;
 public class LevelIntro : MonoBehaviour
 {
 	public float introLength = 2f;
+	public float rotateAngle = 360f;
+	public float zoomRatio = 3f;
 
 	Vector3 finalPosition;
-	Quaternion finalRotation;
-	float finalSize;
-	Camera cam;
 	GameObject center;
-	float timeRemaining = 0f;
+	float currentTime = 0f;
 	Transform origParent;
 	bool isRotating;
+	float zoomDistance;
 
 	void Awake()
     {
-		cam = GetComponent<Camera>();
-		finalSize = cam.orthographicSize;
-
 		center = new GameObject();
 		center.transform.SetPositionAndRotation(Vector3.zero, Quaternion.identity);
+		// Save off the parent so we can set it after rotation
 		origParent = transform.parent;
 
 		finalPosition = transform.position;
+		//move camera back ready for zoom
+		transform.position *= zoomRatio;
+		zoomDistance = (transform.position - finalPosition).magnitude;
 
+		// set camera parent as our new center object
 		transform.parent = center.transform;
-		center.transform.Rotate(Vector3.up, -270f);
 
+		// rotate the center object and the camera will follow
+		center.transform.Rotate(Vector3.up, rotateAngle * -1f);
+
+		// make sure we are still looking at the middle of the playfield
 		transform.LookAt(Vector3.zero);
 
-		timeRemaining = introLength;
 		isRotating = true;
     }
 
 	// Update is called once per frame
 	void Update() {
-		if (timeRemaining <= 0) {
-			transform.parent = origParent;
-    		if (isRotating) {
+		if (isRotating) {
+			if (currentTime >= introLength) {
+				// Stop rotating and reset
 				isRotating = false;
+				transform.parent = origParent;
+				Destroy(center);
 
 				transform.position = finalPosition;
 				transform.LookAt(Vector3.zero);
-			}
-		} else {
-    		timeRemaining -= Time.deltaTime;
-		}
-	}
+			} else {
+				currentTime += Time.deltaTime;
 
-	private void LateUpdate() {
-		if (isRotating) {
-			center.transform.Rotate(center.transform.up, (270f / introLength) * Time.deltaTime);
-			transform.LookAt(Vector3.zero);
+				center.transform.Rotate(center.transform.up,
+					(Mathf.Lerp(rotateAngle*2f, 0f, currentTime/introLength) / introLength) * Time.deltaTime);
+
+				transform.LookAt(Vector3.zero);
+				transform.Translate(Vector3.forward * (zoomDistance / introLength) * Time.deltaTime);
+			}
 		}
 	}
 }
