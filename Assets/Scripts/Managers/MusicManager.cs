23613285f9223609ffaab31df78f15e12d2af9f0
Jim LoRusso <jimmy.lorusso@gmail.com>
2019-03-08 10:35:32 -0500

    Add audio manager

---
+++ Assets/Scripts/Managers/MusicManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour
+{
+    public static MusicManager instance = null;
+    public AudioSource[] sequentialClips;
+    public AudioSource loopClip;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            if (loopClip.clip.name == instance.loopClip.clip.name)
+            {
+                Destroy(gameObject);
+                return;
+            }
+            else
+            {
+                Destroy(instance.gameObject);
+                instance = this;
+            }            
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    void Start()
+    {
+        instance.loopClip.loop = true;
+    }
+
+    public void PlayClips()
+    {
+        StartCoroutine(playClips());
+    }
+
+    private IEnumerator playClips()
+    {
+        Debug.Log("Starting audio clips");
+        foreach (AudioSource sequentialClip in instance.sequentialClips)
+        {
+            sequentialClip.Play();
+            yield return new WaitForSeconds(sequentialClip.clip.length);
+        }
+
+        PlayLoopClip(instance.loopClip);
+    }
+
+    public void PlayLoopClip(AudioSource clip)
+    {
+        if (clip != null)
+        {
+            clip.Play();
+        }
+    }
+}
